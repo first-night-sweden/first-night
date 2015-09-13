124add6add17bd1c10a904698c43c95c1846f940
asahellgren <asa@hellosugar.se>
2015-09-13 20:09:39 +0200

    First commit

---
+++ FirstNightSweden/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="FirstNightSweden.MvcApplication" Language="C#" %>
---
+++ FirstNightSweden/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(FirstNightSweden.Startup))]
+namespace FirstNightSweden
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
